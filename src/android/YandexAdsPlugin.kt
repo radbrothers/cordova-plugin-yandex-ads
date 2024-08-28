5f72d45fb8f8132989d313c25c6d9e411aa193eb
creakosta2 <48723521+creakosta2@users.noreply.github.com>
2024-08-28 11:52:32 +0300

    Update YandexAdsPlugin.kt

--- src/android/YandexAdsPlugin.kt
+++ src/android/YandexAdsPlugin.kt
@@ -2,7 +2,7 @@ package io.luzh.cordova.plugin
 
 import android.net.Uri
 import android.util.Log
-import com.demo.stand.R
+import android.R
 import com.yandex.mobile.ads.common.InitializationListener
 import com.yandex.mobile.ads.common.MobileAds.initialize
 import com.yandex.mobile.ads.common.MobileAds.setUserConsent
@@ -133,7 +133,7 @@ class YandexAdsPlugin : CordovaPlugin() {
         val bannerAtTop = options.optBoolean(KEY_BANNER_AT_TOP, false)
         val bannerSize = options.optJSONObject(KEY_BANNER_SIZE)
 
-        val intreamContentUrl = Uri.parse("android.resource://" + cordova.context.packageName + "/" + R.raw.jc.mp4).toString()
+        val intreamContentUrl = Uri.parse("android.resource://" + cordova.context.packageName + "/" + R.raw.jc).toString()
 
         bannerAdsHelper = BannerAdsHelper(this, webView, bannerBlockId, bannerAtTop, bannerSize)
         rewardedAdsHelper = RewardedAdsHelper(this, webView, rewardedBlockId)
