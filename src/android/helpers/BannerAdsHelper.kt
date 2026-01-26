ae6548bf9d01c8f7bfe9b3a9f7b286ec298a75ce
radbrothers <radbrozers@gmail.com>
2026-01-26 22:57:42 +0300

    Update BannerAdsHelper.kt

--- src/android/helpers/BannerAdsHelper.kt
+++ src/android/helpers/BannerAdsHelper.kt
@@ -51,8 +51,8 @@ internal class BannerAdsHelper(
                     RelativeLayout.LayoutParams.MATCH_PARENT,
                     RelativeLayout.LayoutParams.WRAP_CONTENT
                 ).apply {
-                    val alignRule = if (bannerAtTop) RelativeLayout.ALIGN_PARENT_START
-                    else RelativeLayout.ALIGN_PARENT_START
+                    val alignRule = if (bannerAtTop) RelativeLayout.ALIGN_PARENT_TOP
+                    else RelativeLayout.ALIGN_PARENT_BOTTOM
 
                     addRule(alignRule)
                 }
@@ -98,7 +98,7 @@ internal class BannerAdsHelper(
 
                 bannerContainerLayout = RelativeLayout(cordovaPlugin.cordova.activity)
 
-                bannerContainerLayout?.gravity = Gravity.LEFT
+                bannerContainerLayout?.gravity = Gravity.BOTTOM
                 bannerContainerLayout?.setBackgroundColor(0x000000)
 
                 val layoutParams = RelativeLayout.LayoutParams(
@@ -106,7 +106,7 @@ internal class BannerAdsHelper(
                     RelativeLayout.LayoutParams.WRAP_CONTENT
                 )
 
-                layoutParams.addRule(RelativeLayout.CENTER_VERTICAL)
+                layoutParams.addRule(RelativeLayout.CENTER_HORIZONTAL)
 
                 bannerContainerLayout?.addView(mBannerAdView, layoutParams)
 
